d6746da82750408aad7814c2954f2222a9a90896
Joan <jflaco85@gmail.com>
2020-03-06 16:35:31 -0500

    OK

--- Pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ Pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,42 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Toast.makeText(this, "Creating app on device", Toast.LENGTH_LONG).show();
     }
+
+    public void Btn_Login(View view){
+        Toast.makeText(this, "You have pressed login button", Toast.LENGTH_SHORT).show();
+    }
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "The app has started", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "The app is on your screen", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "The app has been paused", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "The app has been stopped", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "The app has been restarted", Toast.LENGTH_SHORT).show();
+    }
+    */
 }
