a764e97056fd49702c76339064f2bce58414e808
jflaco85@gmail.com <jflaco85@gmail.com>
2020-03-09 15:30:58 -0500

    New changes

--- Pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ Pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -18,6 +19,16 @@ public class LoginActivity extends AppCompatActivity {
 
     public void Btn_Login(View view){
         Toast.makeText(this, "You have pressed login button", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this,
+                InboxActivity.class);
+        startActivity(intent);
+    }
+    public void Btn_Signup(View view){
+        Toast.makeText(this, "You have pressed sign up button", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,
+                RegisterActivity.class);
+        startActivity(intent);
     }
     /*
     @Override
